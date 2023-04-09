92cef7e4bf4a6d27f619e11c354bc06d624469ee
Michael Martinez <mmartinem7@gmail.com>
2023-04-10 01:01:33 +0200

    add tests for recipes fetching usecase

---
+++ test/get_all_recipes_usecase_test.dart
@@ -0,0 +1,53 @@
+import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fridge_cook/src/data/repositories/in_memory_products_repository.dart';
+import 'package:fridge_cook/src/data/repositories/in_memory_recipes_repository.dart';
+import 'package:fridge_cook/src/domain/entities/product.dart';
+import 'package:fridge_cook/src/domain/entities/recipe.dart';
+import 'package:fridge_cook/src/domain/usecases/get_all_recipes_usecase.dart';
+
+void main() {
+
+  setUp(() async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+  });
+
+  tearDown(() async {
+  });
+
+  test(
+      'Given getAllRecipesUseCase should return recipes',
+      () async {
+    GetAllRecipesUseCase getUserUseCase;
+    _Observer observer;
+    var recipes = [Recipe('banana cake', [''], [''], '', [Product("banana", 1, "")])];
+    var repo = InMemoryRecipesRepository(recipes);
+    getUserUseCase = GetAllRecipesUseCase(repo);
+    observer = _Observer();
+    getUserUseCase.execute(observer, GetAllRecipesUseCaseParams([Product('banana', 1, ''), Product('milk', 1, '')], 1));
+    while (!observer.status['progress'].contains('done')) {
+      await Future.delayed(const Duration(seconds: 1));
+    }
+    expect(observer.status['result'], recipes.toString());
+  });
+}
+
+class _Observer implements Observer<GetAllRecipesUseCaseResponse> {
+  final status = {'progress': 'starting', 'result': ''};
+  @override
+  void onNext(response) {
+    expect(GetAllRecipesUseCaseResponse, response.runtimeType);
+    status['progress'] = 'done';
+    status['result'] = response.recipes.toString();
+  }
+
+  @override
+  void onComplete() {}
+
+  @override
+  void onError(e) {
+    status['progress'] = 'done';
+    status['result'] = 'failed';
+  }
+}
+
